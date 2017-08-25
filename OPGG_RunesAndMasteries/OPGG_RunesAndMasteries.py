32b6aaa393dbd2d90fbe199941ac411765ed0251
Jonathan Tan <jontan_@hotmail.com>
2017-08-25 15:31:05 -0400

    Print simple list of champion names/keys using Champion.GG

--- OPGG_RunesAndMasteries/OPGG_RunesAndMasteries.py
+++ OPGG_RunesAndMasteries/OPGG_RunesAndMasteries.py
@@ -1,1 +1,11 @@
+from bs4 import BeautifulSoup
+import re
+import urllib.request
 
+# Parse champion analytics overview page
+soup = BeautifulSoup(urllib.request.urlopen("https://na.op.gg/champion/statistics"), "html.parser")
+
+# Find all div element containing champions
+for x in soup.find_all("div", attrs={"data-champion-name": re.compile(".*")}):
+    # For now, print out champion name and key
+    print(x["data-champion-name"], x["data-champion-key"])
